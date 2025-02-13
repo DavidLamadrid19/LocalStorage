3d0f9aa49d0160fe7ef6c46f6b3486ddfea4e484
DavidLamadrid19 <david14solar@gmail.com>
2025-02-13 09:41:36 -0500

    Add files via upload

---
+++ paghtml/proceso.js
@@ -0,0 +1,8 @@
+localStorage.setItem("Nombre","David")
+localStorage.setItem("Apellido","Lamadrid")
+localStorage.setItem("Edad",18)
+localStorage.setItem("Sisben",false)
+localStorage.setItem("Genero","Masculino")
+localStorage.setItem("Estatura",1.75)
+//Lista ["David","Lamadrid","18", "Sisben","Masculino", "1.75" ]
+
